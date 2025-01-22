de60af37734c6629c5dbacba0cc24967af9bfbbe
Rahul <rahulwp24@gmail.com>
2025-01-22 22:01:58 +0600

    site depoly firebase

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://b10-a12-server-topaz.vercel.app",
 });
 const useAxiosSecure = () => {
   return axiosSecure;
